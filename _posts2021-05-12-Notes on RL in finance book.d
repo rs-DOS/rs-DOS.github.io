451e4cc0deda9aaa987d62a68144727c8d9bde79
Rushabh Doshi <rushabhdoshi94@gmail.com>
2021-12-05 15:18:53 +0530

    Create _posts2021-05-12-Notes on RL in finance book.d

---
+++ _posts2021-05-12-Notes on RL in finance book.d
@@ -0,0 +1,10 @@
+### These are my notes as I read through the [Foundations of reinforcement learning book](https://github.com/TikhonJelvis/RL-book) by [Aswhin Rao](https://www.linkedin.com/in/ashwin2rao/) and 
+[Tikhon Jelvis](https://www.linkedin.com/in/tikhon-jelvis/).
+
+## Chapter 1
+The first chapter basically about coding and designing programs. The book has code samples (written in python) within it with explanations of the concepts relevant to RL.
+All the code used in the book is available on the GitHub page. 
+
+## Chapter 2
+Defines basic concepts of sequential decision making. <br>
+Markov states - they have the property that the probabilities of state *S<sub>t + 1</sub>* only depends on the current state *S<sub>t</sub>*   
